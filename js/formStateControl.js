e0a0963ee8a13599e7a548818ba22bb45cb0b750
지훈 <yamoog@naver.com>
2020-09-20 01:45:58 +0900

    로그인 전송 조건(유효성 검사) 스크립트 작성

--- js/formStateControl.js
+++ js/formStateControl.js
@@ -12,20 +12,39 @@
   }
 
   /**
+   * @constant
+   * 폼 유효성 검사 상태
+   */
+  const validState = {
+    email: false,
+    password: false
+  }
+
+  /**
+   * @var
+   * 모듈 내 접근 가능한 지역 변수
+   */
+  let loginInputWrappers = []
+  let buttonPassword = null
+  let buttonLogin = null
+
+  /**
    * 초기화 함수
    * @function
    */
   function init() {
-    const loginInputWrappers = document.querySelectorAll('div[class*="login-"], div[class*="save-"]')
+    loginInputWrappers = Array.from(document.querySelectorAll('div[class*="login-"], div[class*="save-"]'))
 
-    Array.from(loginInputWrappers).forEach((wrapper) => {
+    loginInputWrappers.forEach((wrapper) => {
       const input = wrapper.querySelector('input')
       input.addEventListener('focus', handleInputFocus.bind(wrapper))
       input.addEventListener('blur', handleInputBlur.bind(wrapper))
     })
 
-    const buttonPassword = document.querySelector('.button-password')
+    buttonPassword = document.querySelector('.button-password')
     buttonPassword.addEventListener('click', handleToggleButtonPassword)
+
+    buttonLogin = document.querySelector('.button-login')
   }
 
   /**
@@ -56,18 +75,94 @@
    */
   function handleInputBlur(e) {
     const { target: input } = e
+    const inputWrapper = input.parentNode
+
+    /* 유효성 검사 ------------------------------------------------------------------- */
 
     if (input.type.includes('email')) {
       input.removeAttribute('placeholder')
+      if (!isValidEmailFormat(input)) {
+        inputWrapper.classList.add('is--invalid')
+        validState.email = false
+      } else {
+        inputWrapper.classList.remove('is--invalid')
+        inputWrapper.classList.add('is--valid')
+        validState.email = true
+      }
     }
-    
-    if (input.value.length === 0) {
-      this.classList.remove(STATE_CLASSES.focus)
+
+    if (input.type.includes('password')) {
+      console.log(isValidPasswordFormat(input))
+      if (!isValidPasswordFormat(input)) {
+        inputWrapper.classList.add('is--invalid')
+        inputWrapper.classList.remove('is--focus')
+        validState.password = false
+      } else {
+        inputWrapper.classList.remove('is--invalid')
+        inputWrapper.classList.remove('is--focus')
+        inputWrapper.classList.add('is--valid')
+        validState.password = true
+      }
     }
     
     if(input.type.includes('checkbox')) {
-      input.nextElementSibling.style.cssText = ``
+      input.nextElementSibling.style.cssText = ''
     }
+
+    if (input.value.length === 0) {
+      this.classList.remove(STATE_CLASSES.focus)
+    }
+
+    /* 전송 버튼 활성/비활성 처리 ---------------------------------------------------------- */
+
+    const isAllInputFilled = checkAllInputsFilled(loginInputWrappers.filter(input => input.className.includes('login-')))
+    
+    if (isAllInputFilled && validState.email && validState.password) {
+      changeLoginButtonSubmitEnable()
+    } else {
+      changeLoginButtonSubmitDisable()
+    }
+  }
+
+  /**
+   * 이메일 유효성 검사 함수
+   * @function
+   * @param {HTMLInputElement} input 이메일 인풋 요소
+   */
+  function isValidEmailFormat(input) {
+    const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ig
+
+    return reg.test(input.value)
+  }
+
+  /**
+   * 패스워드 유효성 검사 함수
+   * @function
+   * @param {HTMLInputElement} input 패스워드 인풋 요소
+   */
+  function isValidPasswordFormat(input) {
+    const reg = /(?=.*\d)(?=.*[a-z]).{6,}/
+    return reg.test(input.value)
+  }
+
+  /**
+   * 로그인 전송 버튼 활성 상태로 변경하는 함수
+   * @function
+   */
+  function changeLoginButtonSubmitEnable() {
+    const buttonLoginWrapper = buttonLogin.parentNode
+    buttonLoginWrapper.classList.remove('is--disabled')
+    buttonLogin.removeAttribute('disabled')
+  }
+
+  /**
+   * 로그인 전송 버튼 비활성 상태로 변경하는 함수
+   * @function
+   */
+  function changeLoginButtonSubmitDisable() {
+    const buttonLoginWrapper = buttonLogin.parentNode
+    buttonLoginWrapper.classList.add('is--disabled')
+    buttonLogin.setAttribute('disabled', 'disabled')
   }
 
   /**
@@ -111,5 +206,19 @@
     button.setAttribute('aria-label', '패스워드 보기')
   }
 
+  /**
+   * 이메일, 패스워드 값이 채워졌는지 검사하는 함수
+   * @function
+   * @param {HTMLDivElement} inputWrapper 이메일, 패스워드 래퍼 요소
+   */
+  function checkAllInputsFilled(inputWrapper) {
+    return inputWrapper.every((wrapper) => {
+      const input = wrapper.querySelector('input')
+      return input.value
+    })
+  }
+
+  // 문서 요소에 접근 가능한 시점에 init 함수 실행
   document.addEventListener('DOMContentLoaded', init)
+  
 })()
